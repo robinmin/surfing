a8cbba0ffd0e861926b2cbe01462fd66343d5d2e
robinmin <luonbin@hotmail.com>
2025-10-23 23:10:48 -0700

    fix: fix i18n issues

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,99 +1,94 @@
 import { getPermalink, getAsset } from './permalinks';
-import { getServerTranslation, DEFAULT_LANGUAGE, type SupportedLanguage } from '~/i18n';
 
-export function getLocalizedHeaderData(lang: SupportedLanguage = DEFAULT_LANGUAGE) {
-  const t = getServerTranslation(lang);
-
+export function getLocalizedHeaderData() {
   return {
     links: [
       {
-        text: t('nav.content'),
+        textKey: 'nav.content',
         links: [
           {
-            text: t('nav.browse'),
+            textKey: 'nav.browse',
             href: getPermalink('/browse'),
           },
           {
-            text: t('nav.articles'),
+            textKey: 'nav.articles',
             href: getPermalink('/articles'),
           },
           {
-            text: t('nav.showcase'),
+            textKey: 'nav.showcase',
             href: getPermalink('/showcase'),
           },
           {
-            text: t('nav.documents'),
+            textKey: 'nav.documents',
             href: getPermalink('/documents'),
           },
           {
-            text: t('nav.cheatsheets'),
+            textKey: 'nav.cheatsheets',
             href: getPermalink('/cheatsheets'),
           },
         ],
       },
       {
-        text: t('nav.cheatsheets'),
+        textKey: 'nav.cheatsheets',
         href: getPermalink('/cheatsheets'),
       },
       {
-        text: t('nav.about'),
+        textKey: 'nav.about',
         href: getPermalink('/about'),
       },
     ],
-    actions: [{ text: t('nav.explore'), href: '/browse', icon: 'tabler:compass' }],
+    actions: [{ textKey: 'nav.explore', href: '/browse', icon: 'tabler:compass' }],
   };
 }
 
-export function getLocalizedFooterData(lang: SupportedLanguage = DEFAULT_LANGUAGE) {
-  const t = getServerTranslation(lang);
-
+export function getLocalizedFooterData() {
   return {
     links: [
       {
-        title: t('footer.sections.content'),
+        titleKey: 'footer.sections.content',
         links: [
-          { text: t('nav.browse'), href: '/browse' },
-          { text: t('nav.articles'), href: '/articles' },
-          { text: t('nav.showcase'), href: '/showcase' },
-          { text: t('nav.documents'), href: '/documents' },
-          { text: t('nav.cheatsheets'), href: '/cheatsheets' },
+          { textKey: 'nav.browse', href: '/browse' },
+          { textKey: 'nav.articles', href: '/articles' },
+          { textKey: 'nav.showcase', href: '/showcase' },
+          { textKey: 'nav.documents', href: '/documents' },
+          { textKey: 'nav.cheatsheets', href: '/cheatsheets' },
         ],
       },
       {
-        title: t('footer.sections.platform'),
+        titleKey: 'footer.sections.platform',
         links: [
-          { text: t('footer.links.aiIntegration'), href: '#' },
-          { text: t('footer.links.obsidianSupport'), href: '#' },
-          { text: t('footer.links.rssFeeds'), href: '/rss.xml' },
+          { textKey: 'footer.links.aiIntegration', href: '#' },
+          { textKey: 'footer.links.obsidianSupport', href: '#' },
+          { textKey: 'footer.links.rssFeeds', href: '/rss.xml' },
         ],
       },
       {
-        title: t('footer.sections.resources'),
+        titleKey: 'footer.sections.resources',
         links: [
-          { text: t('footer.links.documentation'), href: '#' },
-          { text: t('footer.links.contentGuidelines'), href: '#' },
-          { text: t('footer.links.apiReference'), href: '#' },
-          { text: t('footer.links.status'), href: '#' },
+          { textKey: 'footer.links.documentation', href: '#' },
+          { textKey: 'footer.links.contentGuidelines', href: '#' },
+          { textKey: 'footer.links.apiReference', href: '#' },
+          { textKey: 'footer.links.status', href: '#' },
         ],
       },
       {
-        title: t('footer.sections.community'),
+        titleKey: 'footer.sections.community',
         links: [
-          { text: t('nav.about'), href: '/about' },
-          { text: t('footer.links.contributors'), href: '#' },
-          { text: t('footer.links.github'), href: 'https://github.com/robin/surfing' },
-          { text: t('footer.links.support'), href: '#' },
+          { textKey: 'nav.about', href: '/about' },
+          { textKey: 'footer.links.contributors', href: '#' },
+          { textKey: 'footer.links.github', href: 'https://github.com/robin/surfing' },
+          { textKey: 'footer.links.support', href: '#' },
         ],
       },
     ],
     secondaryLinks: [
-      { text: t('common.terms'), href: getPermalink('/terms') },
-      { text: t('common.privacyPolicy'), href: getPermalink('/privacy') },
+      { textKey: 'common.terms', href: getPermalink('/terms') },
+      { textKey: 'common.privacyPolicy', href: getPermalink('/privacy') },
     ],
     socialLinks: [
       { ariaLabel: 'RSS All Content', icon: 'tabler:rss', href: getAsset('/rss.xml') },
       { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/robin/surfing' },
     ],
-    footNote: t('footer.tagline'),
+    footNoteKey: 'footer.tagline',
   };
 }
